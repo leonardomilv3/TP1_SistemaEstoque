81dcc6550ccbc32ad4c5a4019c7567874ee65f77
Andre Alves <andremralves@gmail.com>
2023-12-03 20:09:26 -0300

    test: falsificacao listar fornecedores

---
+++ src/main/java/com/sistemaestoque/app/Fornecedores.java
@@ -0,0 +1,15 @@
+package com.sistemaestoque.app;
+
+import java.util.ArrayList;
+
+public class Fornecedores {
+	private ArrayList<Fornecedor> fornecedores;
+
+	public Fornecedores() {
+		fornecedores = new ArrayList<Fornecedor>();
+	}
+
+  public ArrayList<Fornecedor> listarFornecedores() {
+    return fornecedores;
+  }
+}
