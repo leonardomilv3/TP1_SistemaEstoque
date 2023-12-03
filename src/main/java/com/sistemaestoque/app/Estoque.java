30fa40f81bd89ef869a5cf151583b61b3cd60889
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-12-03 17:13:43 -0300

    refactor: armazena quantidade associada a cada produto no estoque

--- src/main/java/com/sistemaestoque/app/Estoque.java
+++ src/main/java/com/sistemaestoque/app/Estoque.java
@@ -1,23 +1,27 @@
 package com.sistemaestoque.app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Estoque {
 
 	private static final int LIMITE_MINIMO = 10;
-	private List<Produto> listaProduto;
+	private Map<Produto, Integer> quantidadeProdutos;
 	private List<Observador> observadores;
 
 	public Estoque() {
-		this.listaProduto = new ArrayList<Produto>();
+		this.quantidadeProdutos = new HashMap<>();
 		this.observadores = new ArrayList<Observador>();
 	}
 
-	public void armazenaProduto(Produto p) {
-		this.listaProduto.add(p);
-		if (produtoComEstoqueBaixo(p)) {
-			String message = notificarObservadoresBaixoEstoque(p);
+	public void armazenaProduto(Produto produto, int quantidade) {
+		if (this.quantidadeProdutos.containsKey(produto)) {
+			int quantidadeExistente = this.quantidadeProdutos.get(produto);
+			this.quantidadeProdutos.put(produto, quantidadeExistente + quantidade);
+		} else {
+			this.quantidadeProdutos.put(produto, quantidade);
 		}
 	}
 
@@ -26,22 +30,22 @@ public class Estoque {
 		return p.getQtdDisponivel() <= LIMITE_MINIMO;
 	}
 
-	public List<Produto> listaProdutosArmazenados() {
-		return this.listaProduto;
+	public Map<Produto, Integer> listaProdutosArmazenados() {
+		return this.quantidadeProdutos;
 	}
-	
+
 	public void adicionarObservador(AlertaEstoqueBaixo alertaEstoqueBaixo) {
 		observadores.add(alertaEstoqueBaixo);
 	}
-	
+
 	public String notificarObservadoresBaixoEstoque(Produto p) {
 		String mensagem = "";
-		
-		for(Observador obs : observadores) {
+
+		for (Observador obs : observadores) {
 			obs.notificarBaixoEstoque(p);
 			mensagem = "Produto: " + p.getNome() + " | Qtd disponível: " + p.getQtdDisponivel();
 		}
-		
+
 		return mensagem;
 	}
 
