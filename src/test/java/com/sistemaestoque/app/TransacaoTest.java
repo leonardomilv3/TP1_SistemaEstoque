8b833a313ab8b1bb1ec6a8de21e4112d291637ce
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-12-03 22:50:09 -0300

    test: parametriza testes de transacao

--- src/test/java/com/sistemaestoque/app/TransacaoTest.java
+++ src/test/java/com/sistemaestoque/app/TransacaoTest.java
@@ -5,27 +5,40 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.sistemaestoque.app.exception.DescricaoEmBrancoException;
 import com.sistemaestoque.app.exception.ValorInvalidoException;
 import java.util.Date;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class TransacaoTest {
 
-  @Test
-  public void testCriacaoTransacao() throws DescricaoEmBrancoException, ValorInvalidoException {
-    Fornecedor fornecedor = new Fornecedor();
-    Fornecedor forn = new Fornecedor(1, "Natura");
-    ;
-    Produto produto =
-        new Produto("Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, forn, new Date());
-    Transacao transacao = new Transacao("TIPO TRANSACAO", produto);
+  @ParameterizedTest
+  @MethodSource("transacaoProdutoProvider")
+  public void testCriacaoTransacao(String tipoTransacao, Produto produto)
+      throws DescricaoEmBrancoException, ValorInvalidoException {
+    Transacao transacao = new Transacao(tipoTransacao, produto);
 
     assertNotNull(transacao.getProduto());
     assertEquals(produto, transacao.getProduto());
 
     assertNotNull(transacao.getTipo());
-    assertEquals("TIPO TRANSACAO", transacao.getTipo());
+    assertEquals(tipoTransacao, transacao.getTipo());
 
     assertNotNull(transacao.getData());
     long diferencaEmMilissegundos = Math.abs(transacao.getData().getTime() - new Date().getTime());
     assertTrue(diferencaEmMilissegundos < 5000);
+  }
+
+  static Stream<Arguments> transacaoProdutoProvider() throws DescricaoEmBrancoException, ValorInvalidoException {
+    Fornecedor fornecedor = new Fornecedor(1, "Natura");
+    Produto produto1 = new Produto("Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, fornecedor, new Date());
+    Produto produto2 = new Produto("Shampoo", "Produto de higiene", "1111", 3.0f, 4.0f, 15, fornecedor, new Date());
+    Produto produto3 = new Produto("Creme", "Produto de cuidados", "2222", 4.0f, 5.0f, 10, fornecedor, new Date());
+
+    return Stream.of(
+        Arguments.of("Compra", produto1),
+        Arguments.of("Venda", produto2),
+        Arguments.of("Troca", produto3));
   }
 }
