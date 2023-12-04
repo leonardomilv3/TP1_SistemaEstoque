51615950be75fcbb53ff81ff56197dc716bb7597
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-12-03 22:35:46 -0300

    test: parametriza testes de categorias

--- src/test/java/com/sistemaestoque/app/CategoriasTest.java
+++ src/test/java/com/sistemaestoque/app/CategoriasTest.java
@@ -1,42 +1,40 @@
 package com.sistemaestoque.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 
 import com.sistemaestoque.app.exception.DuplicadoException;
 import com.sistemaestoque.app.exception.ValorInvalidoException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CategoriasTest {
-  @Test
-  void testAdicionaCategoria() throws ValorInvalidoException, DuplicadoException {
+
+  @ParameterizedTest
+  @MethodSource("categoriasProvider")
+  void testAdicionaCategoria(Categoria categoria, String mensagemEsperada)
+      throws ValorInvalidoException, DuplicadoException {
     Categorias listaCategorias = new Categorias();
-    Categoria novaCategoria = new Categoria("Saúde");
-    String retorno = listaCategorias.adiciona(novaCategoria);
-    assertEquals("Categoria adicionada", retorno);
+    String retorno = listaCategorias.adiciona(categoria);
+    assertEquals(mensagemEsperada, retorno);
   }
 
-  @Test
-  void testAdicionaDuasCategorias() throws ValorInvalidoException, DuplicadoException {
-    Categorias listaCategorias = new Categorias();
-    Categoria novaCategoria = new Categoria("Saúde");
-    Categoria novaCategoria1 = new Categoria("Limpeza");
-    String retorno = listaCategorias.adiciona(novaCategoria);
-    String retorno1 = listaCategorias.adiciona(novaCategoria1);
-    assertEquals("Categoria adicionada", retorno);
-    assertEquals("Categoria adicionada", retorno1);
+  static Stream<Object[]> categoriasProvider() throws ValorInvalidoException {
+    return Stream.of(
+        new Object[] { new Categoria("Saúde"), "Categoria adicionada" },
+        new Object[] { new Categoria("Limpeza"), "Categoria adicionada" },
+        new Object[] { new Categoria("Tecnologia"), "Categoria adicionada" });
   }
 
   @Test
   void testAdicionaCategoriaDuplicada() throws ValorInvalidoException, DuplicadoException {
     Categorias listaCategorias = new Categorias();
     Categoria novaCategoria = new Categoria("Saúde");
-    Categoria novaCategoria1 = new Categoria("Saúde");
     listaCategorias.adiciona(novaCategoria);
-    assertThrowsExactly(
-        DuplicadoException.class,
-        () -> {
-          listaCategorias.adiciona(novaCategoria1);
-        });
+
+    assertThrows(DuplicadoException.class, () -> listaCategorias.adiciona(novaCategoria));
   }
 }
