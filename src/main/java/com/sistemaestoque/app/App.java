fc22c38feea57d5ec43dc373cc8628f0e50c5238
Andre Alves <andremralves@gmail.com>
2023-12-03 22:10:59 -0300

    feat: cadastro de produto

--- src/main/java/com/sistemaestoque/app/App.java
+++ src/main/java/com/sistemaestoque/app/App.java
@@ -5,6 +5,7 @@ import com.sistemaestoque.app.exception.DuplicadoException;
 import com.sistemaestoque.app.exception.ValorInvalidoException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class App {
   private static Categorias categorias;
@@ -25,7 +26,8 @@ public class App {
           System.out.print("\033[H\033[2J");
           System.out.println("Escreva o nome da nova categoria: ");
 
-          String nomeCategoria = scanner.next();
+          scanner.nextLine();
+          String nomeCategoria = scanner.nextLine();
 
           Categoria categoria = new Categoria(nomeCategoria);
           categorias.adiciona(categoria);
@@ -49,6 +51,37 @@ public class App {
         case 3:
           System.out.print("\033[H\033[2J");
           System.out.flush();
+          scanner.nextLine(); // remove '\n'
+                              //
+          System.out.print("CADASTRO DE PRODUTO\n");
+          
+          System.out.print("Nome: ");
+          String nomeProd = scanner.nextLine();
+          System.out.print("Descrição: ");
+          String descricao = scanner.nextLine();
+          System.out.print("Código de barras: ");
+          String codigoBarras = scanner.nextLine();
+          System.out.print("Preço (Custo): ");
+          float precoCusto = scanner.nextFloat();
+          System.out.print("Preço (Venda): ");
+          float precoVenda = scanner.nextFloat();
+          System.out.print("Quantidade: ");
+          int qtdDisponivel = scanner.nextInt();
+          int idFornecedor = 1;
+          System.out.print("Ano do vencimento: ");
+          int dataA = scanner.nextInt();
+          System.out.print("Mês do vencimento: ");
+          int dataM = scanner.nextInt();
+          //scanner.nextLine(); // remove '\n'
+
+          Date dataValidade = new Date();
+          dataValidade.setMonth(dataM);
+          dataValidade.setYear(dataA);
+
+          Produto p = new Produto(nomeProd, descricao, codigoBarras,
+              precoCusto, precoVenda, qtdDisponivel, fornecedoresDb.listarFornecedores().get(0), dataValidade);
+          estoque.armazenaProduto(p);
+          System.out.print("PRODUTO CADASTRADO!\n");
           break;
         case 4:
           System.out.print("\033[H\033[2J");
