a048619fffa62a31934af482a6674412794ec02a
leonardomilv3 <leonardomilov@gmail.com>
2023-12-03 23:09:07 -0300

    test: duplicacao transferencia filiais

--- src/test/java/com/sistemaestoque/app/FilialTest.java
+++ src/test/java/com/sistemaestoque/app/FilialTest.java
@@ -6,7 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.sistemaestoque.app.exception.DescricaoEmBrancoException;
 import com.sistemaestoque.app.exception.ValorInvalidoException;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,8 +27,9 @@ public class FilialTest {
       throws DescricaoEmBrancoException, ValorInvalidoException {
     Filial filial = new Filial("FILIAL 1");
     Fornecedor fornecedor = new Fornecedor();
-    Produto produto = new Produto(
-        "Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, fornecedor, new Date());
+    Produto produto =
+        new Produto(
+            "Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, fornecedor, new Date());
     filial.adicionaProdutos(produto);
 
     assertEquals(1, filial.listaProdutos().size());
@@ -40,25 +43,61 @@ public class FilialTest {
 
     assertEquals("1234", produtoVendido.getCodigoBarras());
   }
-
+  
   @Test
   public void testTranferenciaUmProdutoEntre2Filiais() throws DescricaoEmBrancoException, ValorInvalidoException {
-    Filial filial1 = new Filial("FILIAL 1");
-    Filial filial2 = new Filial("FILIAL 2");
-
-    Produto prod1 = filial1.vendeProduto("1234");
-    filial1.adicionaProdutos(prod1);
-
-    Produto prod2 = filial2.vendeProduto("4321");
-    filial2.adicionaProdutos(prod2);
-
-    filial1.tranfereProdutoParaFilial(filial2, prod2.getNome(), 10);
-
-    int qtdProd1 = prod1.getQtdDisponivel();
-    int qtdProd2 = prod2.getQtdDisponivel();
-
-    assertEquals(10, qtdProd1);
-    assertEquals(30, qtdProd2);
+	  Filial filial1 = new Filial("FILIAL 1");
+	  Filial filial2 = new Filial("FILIAL 2");
+	  
+	  Produto prod1 = filial1.vendeProduto("1234");
+	  filial1.adicionaProdutos(prod1);
+	  
+	  Produto prod2 = filial2.vendeProduto("4321");
+	  filial2.adicionaProdutos(prod2);
+	  
+	  filial1.tranfereProdutoParaFilial(filial2, prod2.getNome(), 10);
+	  
+	  int qtdProd1 = prod1.getQtdDisponivel();
+	  int qtdProd2 = prod2.getQtdDisponivel();
+	  
+	  assertEquals(10, qtdProd1);
+	  assertEquals(30, qtdProd2);
   }
-
+  
+  @Test
+  public void testTranferenciaDoisProdutoEntre2Filiais() throws DescricaoEmBrancoException, ValorInvalidoException {
+	  Filial filial1 = new Filial("FILIAL 1");
+	  Filial filial2 = new Filial("FILIAL 2");
+	  
+	  Produto prod1 = filial1.vendeProduto("1234");
+	  filial1.adicionaProdutos(prod1);
+	  
+	  Produto prod2 = filial2.vendeProduto("4321");
+	  filial2.adicionaProdutos(prod2);
+	  
+	  filial1.tranfereProdutoParaFilial(filial2, prod1.getNome(), 10);
+	  
+	  
+	  Fornecedor fornecedor = new Fornecedor();
+	  Produto prod3 = new Produto("Shampoo", "Produto de limpeza", "0001", 2.0f, 3.0f, 20, fornecedor, new Date());
+	  Produto prod4 = new Produto("Shampoo", "Produto de limpeza", "0002", 2.0f, 3.0f, 20, fornecedor, new Date());
+	  
+	  filial1.adicionaProdutos(prod3);
+	  
+	  filial2.adicionaProdutos(prod4);
+	  
+	  filial1.tranfereProdutoParaFilial(filial2, prod3.getNome(), 15);
+	  
+	  int qtdProd1 = prod1.getQtdDisponivel();
+	  int qtdProd2 = prod2.getQtdDisponivel();
+	  int qtdProd3 = prod3.getQtdDisponivel();
+	  int qtdProd4 = prod4.getQtdDisponivel();
+	  
+	  assertEquals(10, qtdProd1);
+	  assertEquals(30, qtdProd2);
+	  assertEquals(5, qtdProd3);
+	  assertEquals(35, qtdProd4);
+  }
+  
+  
 }
