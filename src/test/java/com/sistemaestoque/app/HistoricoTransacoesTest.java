78929240581337592013e2ae48111cee2298545f
Thiago Gomes <thiagosiqgomes@gmail.com>
2023-12-03 22:45:39 -0300

    test: parametriza testes de historico de transacoes

--- src/test/java/com/sistemaestoque/app/HistoricoTransacoesTest.java
+++ src/test/java/com/sistemaestoque/app/HistoricoTransacoesTest.java
@@ -1,27 +1,41 @@
 package com.sistemaestoque.app;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.sistemaestoque.app.exception.DescricaoEmBrancoException;
 import com.sistemaestoque.app.exception.ValorInvalidoException;
 import java.util.Date;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class HistoricoTransacoesTest {
 
-  @Test
-  public void testCriacaoTransacao() throws DescricaoEmBrancoException, ValorInvalidoException {
-    Fornecedor fornecedor = new Fornecedor(1, "Natura");
-    ;
-    Produto produto =
-        new Produto(
-            "Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, fornecedor, new Date());
-    Transacao transacao = new Transacao("TIPO TRANSACAO", produto);
+  @ParameterizedTest
+  @MethodSource("transacaoProdutoProvider")
+  public void testCriacaoTransacao(String tipoTransacao, Produto produto)
+      throws DescricaoEmBrancoException, ValorInvalidoException {
+    Transacao transacao = new Transacao(tipoTransacao, produto);
     HistoricoTransacoes historicoTransacoes = new HistoricoTransacoes();
     historicoTransacoes.registraTransacao(transacao);
 
     assertNotNull(historicoTransacoes.getTransacoes());
     assertEquals(1, historicoTransacoes.getTransacoes().size());
     assertEquals(produto, historicoTransacoes.getTransacoes().get(0).getProduto());
+  }
+
+  static Stream<Object[]> transacaoProdutoProvider() throws DescricaoEmBrancoException, ValorInvalidoException {
+    Fornecedor fornecedor = new Fornecedor(1, "Natura");
+    Produto produto1 = new Produto("Sabonete", "Produto de limpeza", "0000", 2.0f, 3.0f, 20, fornecedor, new Date());
+    Produto produto2 = new Produto("Shampoo", "Produto de higiene", "1111", 3.0f, 4.0f, 15, fornecedor, new Date());
+    Produto produto3 = new Produto("Creme", "Produto de cuidados", "2222", 4.0f, 5.0f, 10, fornecedor, new Date());
+
+    return Stream.of(
+        new Object[] { "Compra", produto1 },
+        new Object[] { "Venda", produto2 },
+        new Object[] { "Troca", produto3 }
+    // Adicione mais conjuntos de transações e produtos para testar
+    );
   }
 }
